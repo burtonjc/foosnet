2fddf42e34b67963b2af1b71f7b27566cde8da57
Jacob Burton <burtonjc35@gmail.com>
2014-11-23 23:28:10 -0700

    ignoring node_modules from git

--- server/server.js
+++
@@ -1,9 +0,0 @@
-var app = require('./app'),
-    server;
-
-server = app.listen(2001, function() {
-    var host = server.address().address,
-        port = server.address().port;
-
-    console.log('Application running at http://' + host + ':' + port);
-});
